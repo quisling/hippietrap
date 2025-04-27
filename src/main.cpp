c1c7997ad02fae9fe45fefcdaac70f0be7856a9d
Nils Karlsson Quisling <klass2@desu.se>
2025-04-27 19:59:00 +0200

    Baseplate 16 strips working

--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,36 @@
 #include <FastLED.h>
-#define NUM_STRIPS 3
+#define NUM_STRIPS 16
 #define NUM_LEDS_PER_STRIP 7
 #define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
 
-#define DATA1 12
-#define DATA2  1
-#define DATA3  2
+#define DATA1   14
+#define DATA2   13
+#define DATA3   12
+#define DATA4   11
+#define DATA5   10
+#define DATA6   9
+#define DATA7   3
+#define DATA8   8
+#define DATA9   18
+#define DATA10  17
+#define DATA11  16
+#define DATA12  15
+#define DATA13  7
+#define DATA14  6
+#define DATA15  5
+#define DATA16  4
+
 #define COLOR_ORDER GRB
 #define CHIPSET     WS2812B
 
 #define BRIGHTNESS  10
-#define FRAMES_PER_SECOND 10
+#define FRAMES_PER_SECOND 5
 
 CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 
 bool gReverseDirection = false;
 int chaseLed=0;
 bool blinker = true;
-
-
-CRGBPalette16 gPal;
-
-// Fire2012 by Mark Kriegsman, July 2012
-// as part of "Five Elements" shown here: http://youtu.be/knWiGsmgycY
-//// 
-// This basic one-dimensional 'fire' simulation works roughly as follows:
-// There's a underlying array of 'heat' cells, that model the temperature
-// at each point along the line.  Every cycle through the simulation, 
-// four steps are performed:
-//  1) All cells cool down a little bit, losing heat to the air
-//  2) The heat from each cell drifts 'up' and diffuses a little
-//  3) Sometimes randomly new 'sparks' of heat are added at the bottom
-//  4) The heat from each cell is rendered as a color into the leds array
-//     The heat-to-color mapping uses a black-body radiation approximation.
-//
-// Temperature is in arbitrary units from 0 (cold black) to 255 (white hot).
-//
-// This simulation scales it self a bit depending on NUM_LEDS; it should look
-// "OK" on anywhere from 20 to 100 LEDs without too much tweaking. 
-//
-// I recommend running this simulation at anywhere from 30-100 frames per second,
-// meaning an interframe delay of about 10-35 milliseconds.
-//
-// Looks best on a high-density LED setup (60+ pixels/meter).
-//
-//
-// There are two main parameters you can play with to control the look and
-// feel of your fire: COOLING (used in step 1 above), and SPARKING (used
-// in step 3 above).
-//
-// COOLING: How much does the air cool as it rises?
-// Less cooling = taller flames.  More cooling = shorter flames.
-// Default 55, suggested range 20-100 
-#define COOLING  55
-
-// SPARKING: What chance (out of 255) is there that a new spark will be lit?
-// Higher chance = more roaring fire.  Lower chance = more flickery fire.
-// Default 120, suggested range 50-200.
-#define SPARKING 120
-
-
-void Fire2012WithPalette()
-{
-// Array of temperature readings at each simulation cell
-  static uint8_t heat[NUM_LEDS];
-
-  // Step 1.  Cool down every cell a little
-    for( int i = 0; i < NUM_LEDS; i++) {
-      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
-    }
-  
-    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-    for( int k= NUM_LEDS - 1; k >= 2; k--) {
-      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
-    }
-    
-    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
-    if( random8() < SPARKING ) {
-      int y = random8(7);
-      heat[y] = qadd8( heat[y], random8(160,255) );
-    }
-
-    // Step 4.  Map from heat cells to LED colors
-    for( int j = 0; j < NUM_LEDS; j++) {
-      // Scale the heat value from 0-255 down to 0-240
-      // for best results with color palettes.
-      uint8_t colorindex = scale8( heat[j], 240);
-      CRGB color = ColorFromPalette( gPal, colorindex);
-      int pixelnumber;
-      if( gReverseDirection ) {
-        pixelnumber = (NUM_LEDS-1) - j;
-      } else {
-        pixelnumber = j;
-      }
-      leds[pixelnumber] = color;
-    }
-}
 
 void ChaserLed(){
   
@@ -108,8 +43,10 @@ void ChaserLed(){
     }
     
     leds[chaseLed] = CRGB::Black;
+    leds[chaseLed+1] = CRGB::Blue;
+    leds[chaseLed+2] = CRGB::White;
     chaseLed++;
-    if (chaseLed > 100)
+    if (chaseLed > NUM_LEDS)
     {
       chaseLed = 0;
     }
@@ -121,22 +58,20 @@ void setup() {
     FastLED.addLeds<NEOPIXEL, DATA1>(leds, 0, NUM_LEDS_PER_STRIP);
     // tell FastLED there's 60 NEOPIXEL leds on pin 11, starting at index 60 in the led array
     FastLED.addLeds<NEOPIXEL, DATA2>(leds, NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-    // tell FastLED there's 60 NEOPIXEL leds on pin 12, starting at index 120 in the led array
     FastLED.addLeds<NEOPIXEL, DATA3>(leds, 2 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
-
-  // This first palette is the basic 'black body radiation' colors,
-  // which run from black to red to bright yellow to white.
-  gPal = HeatColors_p;
-  
-  // These are other ways to set up the color palette for the 'fire'.
-  // First, a gradient from black to red to yellow to white -- similar to HeatColors_p
-  //   gPal = CRGBPalette16( CRGB::Black, CRGB::Red, CRGB::Yellow, CRGB::White);
-  
-  // Second, this palette is like the heat colors, but blue/aqua instead of red/yellow
-  //   gPal = CRGBPalette16( CRGB::Black, CRGB::Blue, CRGB::Aqua,  CRGB::White);
-  
-  // Third, here's a simpler, three-step gradient, from black to red to white
-  //   gPal = CRGBPalette16( CRGB::Black, CRGB::Red, CRGB::White);
+    FastLED.addLeds<NEOPIXEL, DATA4>(leds, 3 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA5>(leds, 4 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA6>(leds, 5 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA7>(leds, 6 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA8>(leds, 7 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA9>(leds, 8 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA10>(leds, 9 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA11>(leds, 10 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA12>(leds, 11 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA13>(leds, 12 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA14>(leds, 13 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA15>(leds, 14 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
+    FastLED.addLeds<NEOPIXEL, DATA16>(leds, 15 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);FastLED.setBrightness( BRIGHTNESS );
 
 }
 
@@ -145,20 +80,6 @@ void loop()
   // Add entropy to random number generator; we use a lot of it.
   random16_add_entropy( random());
 
-
-  // Fourth, the most sophisticated: this one sets up a new palette every
-  // time through the loop, based on a hue that changes every time.
-  // The palette is a gradient from black, to a dark color based on the hue,
-  // to a light color based on the hue, to white.
-  //
-  //   static uint8_t hue = 0;
-  //   hue++;
-  //   CRGB darkcolor  = CHSV(hue,255,192); // pure hue, three-quarters brightness
-  //   CRGB lightcolor = CHSV(hue,128,255); // half 'whitened', full brightness
-  //   gPal = CRGBPalette16( CRGB::Black, darkcolor, lightcolor, CRGB::White);
-
-
-  //Fire2012WithPalette(); // run simulation frame, using palette colors
   ChaserLed();
   FastLED.show(); // display this frame#include <Arduino.h>
   FastLED.delay(1000 / FRAMES_PER_SECOND);
