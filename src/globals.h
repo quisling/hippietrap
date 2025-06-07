49e99f7e4ca50d835800dd0563536fe2a7138288
Nils Karlsson Quisling <klass2@desu.se>
2025-06-07 14:05:25 +0200

    Added segmentation between small and large parasoll

--- src/globals.h
+++ src/globals.h
@@ -1,7 +1,5 @@
 #pragma once
-#define NUM_STRIPS 12
-#define NUM_LEDS_PER_STRIP 9
-#define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
+#define LARGE_PARASOL false
 
 #define DATA1   14
 #define DATA2   13
@@ -11,6 +9,8 @@
 #define DATA6   9
 #define DATA7   3
 #define DATA8   8
+
+
 #define DATA9   18
 #define DATA10  17
 #define DATA11  16
@@ -21,3 +21,13 @@
 #define DATA16  4
 
 #define CHIPSET     WS2815
+
+#if LARGE_PARASOL 
+#define NUM_STRIPS 16
+#define NUM_LEDS_PER_STRIP 73
+#else
+#define NUM_STRIPS 8
+#define NUM_LEDS_PER_STRIP 108
+#endif
+
+#define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <FastLED.h>
+#include <DFRobot_BMI160.h> // BMI160 library
+#include <Wire.h>      // I2C library
+
 #include "effects.h"
 #include "globals.h"
 
 
 #define BRIGHTNESS  10
 #define FRAMES_PER_SECOND 10
+#define I2C_SDA 4
+#define I2C_SCL 5
+#define I2C_ADDR 0x68
 
 CRGB leds[NUM_LEDS];
+
+//DFRobot_BMI160 bmi160;
 
 int chaseLed=0;
 bool blinker = true;
 
 void setup() {
   delay(3000); // sanity delay
+  Serial.begin(115200); // Start serial at 115200 baud
+  Serial.println("Hello from ESP32-S3!");
+
     // tell FastLED there's 60 NEOPIXEL leds on pin 10, starting at index 0 in the led array
     FastLED.addLeds<NEOPIXEL, DATA1>(leds, 0, NUM_LEDS_PER_STRIP);
     // tell FastLED there's 60 NEOPIXEL leds on pin 11, starting at index 60 in the led array
@@ -23,18 +34,22 @@ void setup() {
     FastLED.addLeds<NEOPIXEL, DATA6>(leds, 5 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
     FastLED.addLeds<NEOPIXEL, DATA7>(leds, 6 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
     FastLED.addLeds<NEOPIXEL, DATA8>(leds, 7 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<NEOPIXEL, DATA9>(leds, 8 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<NEOPIXEL, DATA10>(leds, 9 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<NEOPIXEL, DATA11>(leds, 10 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<NEOPIXEL, DATA12>(leds, 11 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<NEOPIXEL, DATA13>(leds, 12 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<NEOPIXEL, DATA14>(leds, 13 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<NEOPIXEL, DATA15>(leds, 14 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-    FastLED.addLeds<NEOPIXEL, DATA16>(leds, 15 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
-    
+    if (LARGE_PARASOL){
+      FastLED.addLeds<NEOPIXEL, DATA9>(leds, 8 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+      FastLED.addLeds<NEOPIXEL, DATA10>(leds, 9 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+      FastLED.addLeds<NEOPIXEL, DATA11>(leds, 10 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+      FastLED.addLeds<NEOPIXEL, DATA12>(leds, 11 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+      FastLED.addLeds<NEOPIXEL, DATA13>(leds, 12 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+      FastLED.addLeds<NEOPIXEL, DATA14>(leds, 13 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+      FastLED.addLeds<NEOPIXEL, DATA15>(leds, 14 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+      FastLED.addLeds<NEOPIXEL, DATA16>(leds, 15 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+    }
     FastLED.setBrightness( BRIGHTNESS );
-    Serial.begin(115200); // Start serial at 115200 baud
-    Serial.println("Hello from ESP32-S3!");
+    //Wire.begin(I2C_SDA, I2C_SCL); // SDA = GPIO47, SCL = GPIO21
+    //if (bmi160.I2cInit(I2C_ADDR) != BMI160_OK) {
+    //  Serial.println("BMI160 init failed!");
+    //  while (1);
+    //}
 }
 
 void loop()
@@ -43,11 +58,11 @@ void loop()
   random16_add_entropy( random());
 
   //chaserLed(leds, chaseLed, blinker);
-  //wobbleRing(leds, chaseLed);
-  clockLed(leds);
+  wobbleRing(leds, chaseLed);
+  //clockLed(leds);
   FastLED.show(); // display this frame
   FastLED.delay(1000 / FRAMES_PER_SECOND);
-
+  Serial.println("Hello hello");
 }
   
   
